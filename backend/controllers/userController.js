349dc20a6d71f9ccaed67c1a479f2a854e7cd8b6
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-12-29 17:32:05 +0000

    password are now hashed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,12 +25,58 @@ if(user && (await user.matchPassword(password))) {
 }
 })
 
+// Register a new user 
+// GET /api/users
+// public
+const registerUser = asyncHandler(async (req, res) => {
+  const {name, email, password} = req.body
+ 
+ const userExists = await User.findOne({email})
+ 
+ if(userExists) {
+   res.status(400)
+   throw new Error('User already exists')
+ }
+ 
+ const user = await User.create({
+   name,
+   email,
+   password
+ })
+
+ if(user) {
+    res.status(201).json({
+      _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    token: generateToken(user._id)
+    })
+ } else {
+  res.status(400)
+  throw new Error('User not found')
+ }
+
+ })
+
 // Get user profile
 // GET /api/users/profile
 // private
 const getUserProfile = asyncHandler(async (req, res) => {
-res.send('Success')
+const user = await User.findById(req.user._id)
+
+if(user) {
+  res.json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  })
+} else {
+  res.status(404)
+  throw new Error('User not found')
+}
  })
  
 
-export {authUser, getUserProfile}
+export {authUser, getUserProfile, registerUser}
