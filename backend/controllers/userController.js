732b1591fe126c5039ffbe38d61e973a6d711211
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-12-29 16:47:18 +0000

    web tokens added to login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 
@@ -16,7 +17,7 @@ if(user && (await user.matchPassword(password))) {
     name: user.name,
     email: user.email,
     isAdmin: user.isAdmin,
-    token: null
+    token: generateToken(user._id)
   })
 } else {
   res.status(401)
