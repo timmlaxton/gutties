db08fdc915105815635faa25aab529d2e1dffe10
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-12-29 19:17:20 +0000

    added user reducers, actions, constants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,18 +2,24 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer   } from './reducers/productReducers'
+import { userLoginReducer  } from './reducers/userReducers'
 import {cartRedcuer} from './reducers/cartReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartRedcuer
+  cart: cartRedcuer,
+  userLogin: userLoginReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfor')) : null
+
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage}
+  cart: { cartItems: cartItemsFromStorage},
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
