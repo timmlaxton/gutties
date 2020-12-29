034548125476145ce918f0760f0e590996e4c2b1
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-12-29 16:42:09 +0000

    added conrtollers and user login route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,9 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import { getProductById, getProducts } from '../controllers/productController.js'
 
-// Fetch all products
-// GET /api/products
-// public
-
-router.get('/', asyncHandler (async (req, res) => {
-  const products = await Product.find({})
-  
-  res.json(products)
-}))
-
-// Fetch a single products
-// GET /api/products/:id
-// public
-
-router.get('/:id', asyncHandler (async (req, res) => {
-  const product = await Product.findById(req.params.id)
-
-
-  if(product) {
-    res.json(product)
-  } else {
-    res.status(404)
-    throw new Error('Product not found')
-  }
-  
-}))
+router.route('/').get(getProducts)
+router.route('/:id', ).get(getProductById)
 
 
 export default router
