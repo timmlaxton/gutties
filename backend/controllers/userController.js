57f7eb90209b01416b1c1df3ee54d509e583f11a
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-12-29 19:58:17 +0000

    user can login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,27 +3,28 @@ import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 
-// Authenticate user and retrieve token
-// GET /api/users/login
-// public
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
- const {email, password} = req.body
+  const { email, password } = req.body
 
-const user = await User.findOne({email})
+  const user = await User.findOne({ email })
 
-if(user && (await user.matchPassword(password))) {
-  res.json({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    isAdmin: user.isAdmin,
-    token: generateToken(user._id)
-  })
-} else {
-  res.status(401)
-  throw new Error('Invalid email or password')
-}
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
 })
+
 
 // Register a new user 
 // GET /api/users
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,16 +5,16 @@ import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../cons
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
-      type: USER_LOGIN_REQUEST
+      type: USER_LOGIN_REQUEST,
     })
 
     const config = {
       headers: {
         'Content-Type': 'application/json'
-      }
+      },
     }
 
-    const {data} = await axios.post('/api/users/login', [email,password], config)
+    const {data} = await axios.post('/api/users/login', {email,password}, config)
     
     dispatch({
       type: USER_LOGIN_SUCCESS,
