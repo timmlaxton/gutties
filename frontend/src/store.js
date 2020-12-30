875593c87716455ea9400aed6bbf08385a266c5d
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-12-30 08:13:56 +0000

    user can register accountS

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer   } from './reducers/productReducers'
-import { userLoginReducer  } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer  } from './reducers/userReducers'
 import {cartRedcuer} from './reducers/cartReducers'
 
 const reducer = combineReducers({
@@ -10,6 +10,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   cart: cartRedcuer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
