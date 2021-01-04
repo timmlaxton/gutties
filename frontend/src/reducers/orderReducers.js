210bb78c83242a756021f057a1cfa63e8df51f59
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-01-04 13:16:39 +0000

    added order id, reducer, action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,13 +2,13 @@ import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
   ORDER_CREATE_FAIL,
-  
-  
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL
   
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
-  console.log('in order reducer', action)
   switch(action.type) {
     case ORDER_CREATE_REQUEST:
       return {
@@ -21,6 +21,28 @@ export const orderCreateReducer = (state = {}, action) => {
           order: action.payload
         }
         case ORDER_CREATE_FAIL:
+          return {
+            loading: false,
+            error: true,
+            message: action.payload
+          }
+      default: 
+      return state
+  }
+}
+
+export const orderDetailsReducer = (state = {orderItems: [], shippingAddress: {} }, action) => {
+  switch(action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return {
+        loading: true
+      }
+      case ORDER_DETAILS_SUCCESS:
+        return {
+          loading: false,
+          order: action.payload
+        }
+        case ORDER_DETAILS_FAIL:
           return {
             loading: false,
             error: true,
